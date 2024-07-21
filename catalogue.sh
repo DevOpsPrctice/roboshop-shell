164092a4b11ece10e35075c12e49b7815f3db5d7
vamshi Krishna <vamshi.bachu01@gmail.com>
2024-07-20 18:14:23 -0700

    Catalogue Shell Script

--- catalogue.sh
+++ catalogue.sh
@@ -1,5 +1,5 @@
 cp catalogue.service /etc/systemd/system/catalogue.service
-cp cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
@@ -9,10 +9,12 @@ mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
 cd /app
 unzip /tmp/catalogue.zip
+
 cd /app
 npm install
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl start catalogue
+
 dnf install mongodb-mongosh -y
-mongosh --host MONGODB-SERVER-IPADDRESS </app/db/master-data.js
+mongosh --host localhost </app/db/master-data.js
