9951454fc6fe4821058dd6a390f132a8ec0bc8a2
vamshi Krishna <vamshi.bachu01@gmail.com>
2024-08-16 14:02:08 -0700

    Dispatch setup

--- catalogue.sh
+++ catalogue.sh
@@ -4,6 +4,8 @@ NODEJS
 
 PRINT Install MongoDB client
 dnf install mongodb-mongosh -y &>>$LOG_FILE
+echo $?
 
 PRINT Load Master Data
 mongosh --host mongo.dev.vkdevops.online </app/db/master-data.js &>>$LOG_FILE
+echo $?
