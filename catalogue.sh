41e020949c0c16cfb9aaadabcb5c48c79bbb25d0
vamshi Krishna <vamshi.bachu01@gmail.com>
2024-08-16 14:07:44 -0700

    Dispatch setup

--- catalogue.sh
+++ catalogue.sh
@@ -4,8 +4,16 @@ NODEJS
 
 PRINT Install MongoDB client
 dnf install mongodb-mongosh -y &>>$LOG_FILE
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
 
 PRINT Load Master Data
 mongosh --host mongo.dev.vkdevops.online </app/db/master-data.js &>>$LOG_FILE
-echo $?
+if [ $? -eq 0 ]; then
+  echo SUCCESS
+else
+  echo FAILURE
+fi
--- common.sh
+++ common.sh
@@ -12,55 +12,104 @@ NODEJS() {
 
   PRINT Disable NodeJS default version
   dnf module disable nodejs -y &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0 ]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
+
 
   PRINT Enable NodeJS 20  Module
   dnf module enable nodejs:20 -y &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0 ]; then
+     echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   PRINT Install NodeJS
   dnf install nodejs -y &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0 ]; then
+     echo SUCCESS
+   else
+     echo FAILURE
+   fi
 
   PRINT copy Service File
   cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0 ]; then
+     echo SUCCESS
+   else
+     echo FAILURE
+   fi
 
   PRINT copy MongoDB repo file
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0 ]; then
+     echo SUCCESS
+   else
+     echo FAILURE
+   fi
 
   PRINT adding Application User
   useradd roboshop &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0 ]; then
+     echo SUCCESS
+   else
+     echo FAILURE
+   fi
 
   PRINT Cleaning Old content
   rm -rf /app &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0 ]; then
+     echo SUCCESS
+   else
+     echo FAILURE
+   fi
 
   PRINT create App directory
   mkdir /app &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0 ]; then
+     echo SUCCESS
+   else
+     echo FAILURE
+   fi
 
   PRINT Download App Content
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0 ]; then
+     echo SUCCESS
+   else
+     echo FAILURE
+   fi
 
   cd /app
 
   PRINT Extract App Content
   unzip /tmp/${component}.zip &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0 ]; then
+     echo SUCCESS
+   else
+     echo FAILURE
+   fi
 
   cd /app
 
   PRINT Download NodeJS dependencies
   npm install &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0 ]; then
+     echo SUCCESS
+   else
+     echo FAILURE
+   fi
 
   PRINT Start Service
   systemctl daemon-reload &>>$LOG_FILE
   systemctl enable ${component} &>>$LOG_FILE
   systemctl restart ${component} &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0 ]; then
+     echo SUCCESS
+   else
+     echo FAILURE
+   fi
 }
