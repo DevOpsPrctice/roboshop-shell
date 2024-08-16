c27911c0140965147cf3899fa033e4b211cd09b1
vamshi Krishna <vamshi.bachu01@gmail.com>
2024-08-16 13:58:50 -0700

    Dispatch setup

--- catalogue.sh
+++ catalogue.sh
@@ -2,8 +2,8 @@ source common.sh
 component=catalogue
 NODEJS
 
-echo Install MongoDB client
+PRINT Install MongoDB client
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 
-echo Load Master Data
+PRINT Load Master Data
 mongosh --host mongo.dev.vkdevops.online </app/db/master-data.js &>>$LOG_FILE
--- common.sh
+++ common.sh
@@ -10,44 +10,44 @@ PRINT(){
 
 NODEJS() {
 
-  echo Disable NodeJS default version
+  PRINT Disable NodeJS default version
   dnf module disable nodejs -y &>>$LOG_FILE
 
-  echo Enable NodeJS 20  Module
+  PRINT Enable NodeJS 20  Module
   dnf module enable nodejs:20 -y &>>$LOG_FILE
 
-  echo Install NodeJS
+  PRINT Install NodeJS
   dnf install nodejs -y &>>$LOG_FILE
 
-  echo copy Service File
+  PRINT copy Service File
   cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
 
-  echo copy MongoDB repo file
+  PRINT copy MongoDB repo file
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
 
-  echo adding Application User
+  PRINT adding Application User
   useradd roboshop &>>$LOG_FILE
 
-  echo Cleaning Old content
+  PRINT Cleaning Old content
   rm -rf /app &>>$LOG_FILE
 
-  echo create App directory
+  PRINT create App directory
   mkdir /app &>>$LOG_FILE
 
-  echo Download App Content
+  PRINT Download App Content
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>$LOG_FILE
 
   cd /app
 
-  echo Extract App Content
+  PRINT Extract App Content
   unzip /tmp/${component}.zip &>>$LOG_FILE
 
   cd /app
 
-  echo Download NodeJS dependencies
+  PRINT Download NodeJS dependencies
   npm install &>>$LOG_FILE
 
-  echo Start Service
+  PRINT Start Service
   systemctl daemon-reload &>>$LOG_FILE
   systemctl enable ${component} &>>$LOG_FILE
   systemctl restart ${component} &>>$LOG_FILE
